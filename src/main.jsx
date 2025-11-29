9d2d20e7776704d4788ac96ad63604c9b17320f2
riddimbasellc-sys <riddimbasellc@gmail.com>
2025-11-29 14:07:36 -0500

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
+import { SiteSettingsProvider } from '../frontend/src/context/SiteSettingsContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <SiteSettingsProvider>
+        <App />
+      </SiteSettingsProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
